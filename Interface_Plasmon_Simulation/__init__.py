b6fa55c4c0d2bc54b87a4f3273b8b3a079f088fc
Eric R. Hoglund <erh3cq@virginia.edu>
2017-06-08 12:06:13 -0400

    Add package requirements

---
+++ Interface_Plasmon_Simulation/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 11:59:33 2017
+
+@author: Eric Hoglund
+"""
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 12:00:03 2017
+
+@author: Eric Hoglund
+"""
+
+from setuptools import setup
+
+setup(name='Interface Plasmon Simulation',
+      version='0.1',
+      description='Simulation of interface plasmons via multi-slab models',
+      url='https://github.com/erh3cq/Interface_Plasmon_Simulation',
+      author='Eric Hoglund',
+      author_email='erh3cq@virginia.edu',
+      license='',
+      packages=['Interface_Plasmon_Simulation'],
+      zip_safe=False)
