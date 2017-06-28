3894ad1e437aa38d54d8ebcdad493a05fdc55a67
Eric R. Hoglund <erh3cq@virginia.edu>
2017-06-28 15:42:16 -0400

    update plot test

--- Interface_Plasmon_Simulation/plotting_test_script.py
+++ Interface_Plasmon_Simulation/plotting_test_script.py
@@ -11,8 +11,6 @@ import matplotlib.colors as colors
 from scipy.integrate import quad
 from materials import Al, GB, vac, Al2O3
 from create_system import System
-from microscope import Microscope
-from dimensions import Spectrum_dimensions
 from Bulk_Plasmon import bulk_plasmon_double_differential_cross_section
 from normal_incidence_wRetardation import double_differential_cross_section_normalIncidence
 
@@ -20,17 +18,12 @@ system = System(keV=100,
                 q_perpendicular_max=6E10,
                 E_min=5,
                 E_max=20)
-system.configure_traits()
+#system.configure_traits()
 
-#MR_Spectra = double_differential_cross_section_normalIncidence(system.microscope, system.dimensions, [vac, Al], 100*10E-9)
+MR_Spectra = double_differential_cross_section_normalIncidence(system.microscope, system.dimensions, [vac, Al], 100*10E-9)
 
-microscope = Microscope(keV=100)
-scope.configure_traits()
-#print('theta_max: %.4f'%(dimensions.q_perpendicular.max()/scope.k0))
-#print('(111) @',2*np.pi/2.338,'[1/A] \n')
-dimensions = Spectrum_dimensions(q_perpendicular_max=6E10, E_min=5, E_max=20)
-#dimensions.configure_traits()
-MR_Spectra = double_differential_cross_section_normalIncidence(microscope, dimensions, [vac, Al], 100*10E-9)
+##print('theta_max: %.4f'%(dimensions.q_perpendicular.max()/scope.k0))
+##print('(111) @',2*np.pi/2.338,'[1/A] \n')
 
 
 def plot_boundary_markers(spectrum, material):
@@ -46,71 +39,72 @@ def plot_boundary_markers(spectrum, material):
     plt.xlim([spectrum.E.min(),spectrum.E.max()])
 
 
-#fig, ax = plt.subplots()
-##print(MR_Spectra.eps.real)
-#plt.plot(MR_Spectra.eps[1][0].real, label=r'$\epsilon_r q=0$')
-#plt.plot(MR_Spectra.eps[1][200].real, label=r'$\epsilon_r q=200$')
-#plt.plot(MR_Spectra.eps[1][0].conj().imag, label=r'$\epsilon_i q=0$')
-#plt.plot(MR_Spectra.eps[1][200].conj().imag, label=r'$\epsilon_i q=200$')
-#plt.legend()
-#
-#
-#bounds = [dimensions.E.min(), dimensions.E.max(), dimensions.q_perpendicular.min() *10**-10, dimensions.q_perpendicular.max() *10**-10]
-#
-#fig, ax= plt.subplots(2,2)
-#
-#im = ax[0,0].imshow(MR_Spectra.bulk_mode(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
-#           extent=bounds)
-#fig.colorbar(im, ax=ax[0,0], label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
-#ax[0,0].set_title('Bulk')
-#ax[0,0].set_ylabel(r'$q_y [A^-]$')
-#ax[0,0].set_xlabel(r'$E [eV]$')
-#
-#im = ax[0,1].imshow(MR_Spectra.surface_mode(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
-#           extent=bounds)
-#fig.colorbar(im, ax=ax[0,1], label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
-#ax[0,1].set_title('Surface')
-#ax[0,1].set_ylabel(r'$q_y [A^-]$')
-#ax[0,1].set_xlabel(r'$E [eV]$')
-#
-#im = ax[1,0].imshow(MR_Spectra.guidedLight1(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
-#           extent=bounds)
-#fig.colorbar(im, ax=ax[1,0], label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
-#ax[1,0].set_title('Guided light 1')
-#ax[1,0].set_ylabel(r'$q_y [A^-]$')
-#ax[1,0].set_xlabel(r'$E [eV]$')
-#
-#im = ax[1,1].imshow(MR_Spectra.guidedLight2(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
-#           extent=bounds)
-#fig.colorbar(im, ax=ax[1,1], label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
-#ax[1,1].set_title('Guided light 1')
-#ax[1,1].set_ylabel(r'$q_y [A^-]$')
-#ax[1,1].set_xlabel(r'$E [eV]$')
-#
-#
-#
-#fig, ax= plt.subplots()
-#im = ax.imshow(MR_Spectra.total(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
-#           extent=bounds)
+fig, ax = plt.subplots()
+#print(MR_Spectra.eps.real)
+plt.plot(MR_Spectra.eps[1][0].real, label=r'$\epsilon_r q=0$')
+plt.plot(MR_Spectra.eps[1][200].real, label=r'$\epsilon_r q=200$')
+plt.plot(MR_Spectra.eps[1][0].conj().imag, label=r'$\epsilon_i q=0$')
+plt.plot(MR_Spectra.eps[1][200].conj().imag, label=r'$\epsilon_i q=200$')
+plt.legend()
+
+
+
+bounds = [system.dimensions.E.min(), system.dimensions.E.max(), system.dimensions.q_perpendicular.min() *10**-10, system.dimensions.q_perpendicular.max() *10**-10]
+
+fig, ax= plt.subplots(2,2)
+
+im = ax[0,0].imshow(MR_Spectra.bulk_mode(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
+           extent=bounds)
+fig.colorbar(im, ax=ax[0,0], label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
+ax[0,0].set_title('Bulk')
+ax[0,0].set_ylabel(r'$q_y [A^-]$')
+ax[0,0].set_xlabel(r'$E [eV]$')
+
+im = ax[0,1].imshow(MR_Spectra.surface_mode(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
+           extent=bounds)
+fig.colorbar(im, ax=ax[0,1], label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
+ax[0,1].set_title('Surface')
+ax[0,1].set_ylabel(r'$q_y [A^-]$')
+ax[0,1].set_xlabel(r'$E [eV]$')
+
+im = ax[1,0].imshow(MR_Spectra.guidedLight1(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
+           extent=bounds)
+fig.colorbar(im, ax=ax[1,0], label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
+ax[1,0].set_title('Guided light 1')
+ax[1,0].set_ylabel(r'$q_y [A^-]$')
+ax[1,0].set_xlabel(r'$E [eV]$')
+
+im = ax[1,1].imshow(MR_Spectra.guidedLight2(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
+           extent=bounds)
+fig.colorbar(im, ax=ax[1,1], label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
+ax[1,1].set_title('Guided light 1')
+ax[1,1].set_ylabel(r'$q_y [A^-]$')
+ax[1,1].set_xlabel(r'$E [eV]$')
+
+
+
+fig, ax= plt.subplots()
+im = ax.imshow(MR_Spectra.total(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
+           extent=bounds)
+fig.colorbar(im, ax=ax, label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
+ax.set_title('Total')
+ax.set_ylabel(r'$q_y [A^-]$')
+ax.set_xlabel(r'$E [eV]$')
+
+fig, ax= plt.subplots()
+im = ax.imshow(MR_Spectra.total(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
+           extent=bounds, norm=colors.SymLogNorm(linthresh=0.0001))
+fig.colorbar(im, ax=ax, label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
+ax.set_title('log10 Total')
+ax.set_ylabel(r'$q_y [A^-]$')
+ax.set_xlabel(r'$E [eV]$')
+#plot_boundary_markers(dimensions, Al)
+
+fig, ax= plt.subplots()
+im = ax.plot(dimensions.E,np.trapz(MR_Spectra.total(),axis=0,dx=(dimensions.E[1]-dimensions.E[0])))
 #fig.colorbar(im, ax=ax, label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
-#ax.set_title('Total')
-#ax.set_ylabel(r'$q_y [A^-]$')
-#ax.set_xlabel(r'$E [eV]$')
-#
-#fig, ax= plt.subplots()
-#im = ax.imshow(MR_Spectra.total(), origin='lower', aspect='auto', cmap=plt.get_cmap('hot'),
-#           extent=bounds, norm=colors.SymLogNorm(linthresh=0.0001))
-#fig.colorbar(im, ax=ax, label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
-#ax.set_title('log10 Total')
-#ax.set_ylabel(r'$q_y [A^-]$')
-#ax.set_xlabel(r'$E [eV]$')
-##plot_boundary_markers(dimensions, Al)
-#
-#fig, ax= plt.subplots()
-#im = ax.plot(dimensions.E,np.trapz(MR_Spectra.total(),axis=0,dx=(dimensions.E[1]-dimensions.E[0])))
-##fig.colorbar(im, ax=ax, label=r'$\frac{dP^2}{dE dq_y} \left[ eV\ nm^{-1} \right] $')
-#ax.set_title('Total')
-#ax.set_ylabel(r'$q_y [A^-]$')
-#ax.set_xlabel(r'$E [eV]$')
-#
-#plt.show()
+ax.set_title('Total')
+ax.set_ylabel(r'$q_y [A^-]$')
+ax.set_xlabel(r'$E [eV]$')
+
+plt.show()
